630197874a583ac4f7ff4eff8c8f38519322d666
Ingrid Fabiola Garay Gómez <ingridgaray96@gmail.com>
2023-03-15 17:19:40 -0600

    update

---
+++ 00_Introduccion/Hola_Mundo.py
@@ -0,0 +1,10 @@
+print("Hola Mundo")
+
+#Todos los arvhivos nuevo dentro de una carpeta van con minusculas y palabras separadas por guión bajo
+# Todo archivo debe de tener comentarios
+
+#TAREA 1
+# hacer una modificación de cada uno de los elementos, realizando la misma operación,
+# En lugar de los nombres el resultado
+# Operaciones y tipos
+# Ejercicio_1.py
