accc0c9f6aeb4be6b8e27cd615253f854fec2681
Ingrid Fabiola Garay Gómez <ingridgaray96@gmail.com>
2023-03-22 17:40:57 -0600

    update

--- 01_Expresiones/Expresion_Caracter.py
+++
@@ -1,9 +0,0 @@
-import re
-
-frase = "Tengo 2 hijos que tienen 15 y 11 años"
-patron = '[0-9]+' #Esta es una expresión regular
-nuevo= re.findall(patron, frase)
-
-print(nuevo)
-print(type(nuevo))
-
