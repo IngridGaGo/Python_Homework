24c1d2007c74c0ffc5ad1e5823df382d67f3512e
Ingrid Fabiola Garay Gómez <ingridgaray96@gmail.com>
2023-03-22 16:06:21 -0600

    update

---
+++ 01_Expresiones/Ejercicio_2.py
@@ -0,0 +1,187 @@
+### Operador +.
+a = 7 + 3
+print('Operador +: a=7+3 # Resultado: ',a)                                      # 10
+print('Operador +: a=7+3 # Tipo: ',type(a),'\n')                                # <class 'int'>
+a = 5
+b = 3
+c = a + b
+print('Operador +: a = 5 b = 3 c = a + b #',c)                                  # 8
+print('Operador +: a = 5 b = 3 c = a + b # Tipo: ', type(c),'\n')               # <class 'int'>
+print('Operador +: 3+4 # Resultado: ',3+4)                                      # 7
+print('Operador +: 3+4 # Tipo: ',type(3+4),'\n')                                # <class 'int'>
+
+
+### Operador -.
+a = 6 - 2
+print('Operador -: 6 - 2 # Resultado: ', a)                                     # 4
+print('Operador -: 6 - 2 # Tipo: ', type(a),'\n')                               # <class 'int'>
+a = 5
+b = 3
+c = a - b
+print('Operador -: a = 5 b = 3 c = a - b # Resultado: ',c)                      # 2
+print('Operador -: a = 5 b = 3 c = a - b # Tipo: ',type(c),'\n')                # <class 'int'>
+print('Operador -: 2-6 # Resultado: ',2-6)                                      # -4
+print('Operador -: 2-6 # Tipo: ',type(2-6),'\n')                                # <class 'int'>
+
+
+### Operador *.
+a = 3 * 4
+print('Operador *: a = 3 * 4 # Resultado: ',a)                                  # 12
+print('Operador *: a = 3 * 4 # Tipo: ',type(a),'\n')                            # <class 'int'>
+a = 6
+b = 3
+c = a * b
+print('Operador *: a = 6 b = 3 c = a * b  # Resultado: ',c)                     # 18
+print('Operador *: a = 6 b = 3 c = a * b  # Tipo: ',type(c),'\n')               # <class 'int'>
+print('Operador *: 5*7 # Resultado: ',5*7)                                      # 35
+print('Operador *: 5*7 # Tipo: ',type(5*7),'\n')                                # <class 'int'>
+
+
+### Operador /.
+a = 6 / 2
+print('Operador /: a = 6 / 2 # Resultado: ',a)                                  # 3.0
+print('Operador /: a = 6 / 2 # Tipo: ',type(a),'\n')                            # <class 'float'>
+a = 5
+b = 3
+c = a / b
+print('Operador /: a = 5 b = 3 c = a / b # Resultado: ',c)                      # 1.6666666666666667
+print('Operador /: a = 5 b = 3 c = a / b # Tipo: ',type(c),'\n')                # <class 'float'>
+print('Operador /: 10/3 # Resultado: ',10/3)                                    # 3.3333333333333335
+print('Operador /: 10/3 # Tipo: ',type(10/3),'\n')                              # <class 'float'>
+
+
+### Operador %.
+a = 8 % 4
+print('Operador %: a = 8 % 4 # Resultado: ',a)                                  # 0
+print('Operador %: a = 8 % 4 # Tipo: ',type(a),'\n')                            # <class 'int'>
+a = 9
+b = 2
+c = a % b
+print('Operador %: a = 9 b = 2 c = a % b # Resultado: ',c)                      # 1
+print('Operador %: c # Tipo: ',type(c),'\n')                                    # <class 'int'>
+print('Operador %: 6%3 # Resultado: ',6%3)                                      # 0
+print('Operador %: 6%3 # Tipo: ',type(6%3),'\n')                                # <class 'int'>
+
+
+### Operador **.
+a = 3 ** 3
+print('Operador **: a = 3 ** 3 # Resultado: ',a)                                # 27
+print('Operador **: a = 3 ** 3 # Tipo: ',type(a),'\n')                          # <class 'int'>
+a = 2 
+b = 4
+c = a ** b
+print('Operador **: a = 2 b = 4 c = a ** b # Resultado: ',c)                    # 16
+print('Operador **: a = 2 b = 4 c = a ** b # Tipo: ',type(c),'\n')              # <class 'int'>
+print('Operador **: 4**3 # Resultado: ',4**3)                                   # 64
+print('Operador **: 4**3 # Tipo: ',type(4**3),'\n')                             # <class 'int'>
+
+
+### Operador <.
+a = 3 < 3
+print('Operador <: a = 3 < 3 # Resultado: ',a)                                  # False
+print('Operador <: a = 3 < 3 # Tipo: ',type(a),'\n')                            # <class 'bool'>
+a = 2
+b = 4
+c = a < b
+print('Operador <: a = 2 b = 4 c = a < b # Resultado: ',c)                      # True
+print('Operador <: a = 2 b = 4 c = a < b # Tipo: ',type(c),'\n')                # <class 'bool'>
+print('Operador <: 4 < 3 # Resultado: ',4<3)                                    # False
+print('Operador <: 4 < 3 # Tipo: ',type(4<3),'\n')                              # <class 'bool'>
+
+
+### Operador >.
+a = 4 > 2
+print('Operador >: a = 4 > 2 # Resultado: ',a)                                  # True
+print('Operador >: a = 4 > 2 # Tipo: ',type(a),'\n')                            # <class 'bool'>
+a = 5
+b = 7
+c = a > b
+print('Operador >: a = 5 b = 7 c = a > b # Tipo: ',c)                           # False
+print(type(c),'\n')                                                             # <class 'bool'>
+print('Operador >: 9>2 # Resultado: ',9>2)                                      # True
+print('Operador >: 9>2 # Tipo: ', type(9>2),'\n')                               # <class 'bool'>
+
+
+### Operador ==.
+a = 5 == 5
+print('Operador ==: a = 5 == 5 # Resultado: ',a)                                # True
+print('Operador ==: a = 5 == 5 # Tipo: ',type(a),'\n')                          # <class 'bool'>
+a = 6
+b = 9
+c = a == b
+print('Operador -: a = 6 b = 9 c = a == b # Resultado: ',c)                     # False
+print('Operador -: a = 6 b = 9 c = a == b # Tipo: ',type(c),'\n')               # <class 'bool'>
+print('Operador -: 6==2 # Resultado: ',6==2)                                    # False
+print('Operador -: 6==2 # Tipo: ',type(6==2),'\n')                              # <class 'bool'>
+
+
+### Operador !=.
+a = 4 != 2
+print('Operador !=: a = 4 != 2 # Resultado: ',a)                                # True
+print('Operador !=: a = 4 != 2 # Tipo: ',type(a),'\n')                          # <class 'bool'>
+a = 5
+b = 3
+c = a != b
+print('Operador !=: a = 5 b = 3 c = a != b # Resultado: ',c)                    # True
+print('Operador !=: a = 5 b = 3 c = a != b # Tipo: ',type(c),'\n')              # <class 'bool'>
+print('Operador !=: 8!=8 # Resultado: ',8!=8)                                   # False
+print('Operador !=: 8!=8 # Tipo: ',type(8!=8),'\n')                             # <class 'bool'>
+
+
+### Operador <=.
+a = 5 <= 3
+print('Operador <=: a = 5 <= 3 # Resultado: ',a)                                # False
+print('Operador <=: a = 5 <= 3 # Tipo: ',type(a),'\n')                          # <class 'bool'>
+x = 7
+y = 5
+z = x <= y
+print('Operador <=: x = 7 y = 5 z = x <= y # Resultado: ',z)                    # False
+print('Operador <=: x = 7 y = 5 z = x <= y # Tipo: ',type(z),'\n')              # <class 'bool'>
+print('Operador <=: 9<=4 # Resultado: ',9<=4)                                   # False
+print('Operador <=: 9<=4 # Tipo: ',type(9<=4),'\n')                             # <class 'bool'>
+
+
+### Operador >=.
+a = 2 >= 8
+print('Operador >=: a = 2 >= 8 # Resultado: ',a)                                # False
+print('Operador >=: a = 2 >= 8 # Tipo: ',type(a),'\n')                          # <class 'bool'>
+a = 3
+b = 4
+c = a >= b
+print('Operador >=: a = 3 b = 4 c = a >= b # Resultado: ',c)                    # False
+print('Operador >=: a = 3 b = 4 c = a >= b # Tipo: ',type(c),'\n')              # <class 'bool'>
+print('Operador >=: 7>=3 # Resultado: ',7>=3)                                   # True
+print('Operador >=: 7>=3 # Tipo: ',type(7>=3),'\n')                             # <class 'bool'>
+
+
+### Operador and.
+print('Operador and: 4-1==3 and 5>6 # Resultado: ', 4-1==3 and 5>6)             # False
+print('Operador and: 4-1==3 and 5>6 # Tipo: ',type(4-1==3 and 5>6),'\n')        # <class 'bool'>
+print('Operador and: 6+7 > 11 and 3==3 # Resultado: ',6+7 > 11 and 3==3)        # True
+print('Operador and: 6+7 > 11 and 3==3 # Tipo: ',type(6+7 > 11 and 3==3),'\n')  # <class 'bool'>
+
+
+### Operador or.
+print('Operador or: 4-1==3 or 5>6 # Resultado: ',4-1==3 or 5>6)                 # True
+print('Operador or: 4-1==3 or 5>6 # Tipo: ',type(4-1==3 or 5>6),'\n')           # <class 'bool'>
+print('Operador or: 6+7 > 11 or 3==3 # Resultado: ',6+7 > 11 or 3==3)           # True
+print('Operador or: 6+7 > 11 or 3==3 # Tipo: ',type(6+7 > 11 or 3==3),'\n')     # <class 'bool'>
+
+
+### Operador not.
+print('Operador not: not 5>6 # Resultado: ',not 5>6)                            # True
+print('Operador not: not 5>6 # Tipo: ',type(not 5>6),'\n')                      # <class 'bool'>
+print('Operador not: not 5>4 # Resultado: ',not 5>4)                            # False
+print('Operador not: not 5>4 # Tipo: ',type(not 5>4),'\n')                      # <class 'bool'>
+
+
+## Expresiones de carácter
+import re
+
+frase = "Tengo 2 hijos que tienen 15 y 11 años"
+patron = '[0-9]+' #Esta es una expresión regular
+nuevo= re.findall(patron, frase)
+
+print(nuevo)
+print(type(nuevo))
+
